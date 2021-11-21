76acba2f5fd4789b5802b78f8c344ff7c55cffba
Bryanskaya <bryanskayakatya@yandex.ru>
2021-11-22 01:03:15 +0300

    ADDED: lab 4

---
+++ lab04/lab04/Form1.Designer.cs
@@ -0,0 +1,372 @@
+﻿namespace lab04
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.inputA = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.inputB = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.inputReverse = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.inputS = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.inputM = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.error = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.resTime = new System.Windows.Forms.TextBox();
+            this.resEvent = new System.Windows.Forms.TextBox();
+            this.label13 = new System.Windows.Forms.Label();
+            this.label14 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(169, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(233, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Лабораторная работа № 4";
+            // 
+            // inputA
+            // 
+            this.inputA.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.inputA.Location = new System.Drawing.Point(62, 25);
+            this.inputA.Name = "inputA";
+            this.inputA.Size = new System.Drawing.Size(37, 26);
+            this.inputA.TabIndex = 0;
+            this.inputA.Text = "0";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(0, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(99, 20);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Генератор";
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.splitContainer1.Location = new System.Drawing.Point(27, 55);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.label11);
+            this.splitContainer1.Panel1.Controls.Add(this.inputB);
+            this.splitContainer1.Panel1.Controls.Add(this.label4);
+            this.splitContainer1.Panel1.Controls.Add(this.inputA);
+            this.splitContainer1.Panel1.Controls.Add(this.label3);
+            this.splitContainer1.Panel1.Controls.Add(this.label2);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.label12);
+            this.splitContainer1.Panel2.Controls.Add(this.label10);
+            this.splitContainer1.Panel2.Controls.Add(this.inputReverse);
+            this.splitContainer1.Panel2.Controls.Add(this.label9);
+            this.splitContainer1.Panel2.Controls.Add(this.inputS);
+            this.splitContainer1.Panel2.Controls.Add(this.label6);
+            this.splitContainer1.Panel2.Controls.Add(this.label5);
+            this.splitContainer1.Panel2.Controls.Add(this.inputM);
+            this.splitContainer1.Panel2.Controls.Add(this.label7);
+            this.splitContainer1.Size = new System.Drawing.Size(509, 159);
+            this.splitContainer1.SplitterDistance = 179;
+            this.splitContainer1.TabIndex = 1;
+            // 
+            // inputB
+            // 
+            this.inputB.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.inputB.Location = new System.Drawing.Point(62, 67);
+            this.inputB.Name = "inputB";
+            this.inputB.Size = new System.Drawing.Size(37, 26);
+            this.inputB.TabIndex = 3;
+            this.inputB.Text = "5";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(3, 73);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(46, 17);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "b   = ";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(3, 31);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(46, 17);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "a   = ";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label10.Location = new System.Drawing.Point(269, 110);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(21, 17);
+            this.label10.TabIndex = 13;
+            this.label10.Text = "%";
+            // 
+            // inputReverse
+            // 
+            this.inputReverse.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.inputReverse.Location = new System.Drawing.Point(184, 104);
+            this.inputReverse.Name = "inputReverse";
+            this.inputReverse.Size = new System.Drawing.Size(79, 26);
+            this.inputReverse.TabIndex = 12;
+            this.inputReverse.Text = "0";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label9.Location = new System.Drawing.Point(4, 110);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(157, 17);
+            this.label9.TabIndex = 11;
+            this.label9.Text = "Обратная связь   = ";
+            // 
+            // inputS
+            // 
+            this.inputS.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.inputS.Location = new System.Drawing.Point(65, 67);
+            this.inputS.Name = "inputS";
+            this.inputS.Size = new System.Drawing.Size(37, 26);
+            this.inputS.TabIndex = 7;
+            this.inputS.Text = "1";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label6.Location = new System.Drawing.Point(6, 73);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(46, 17);
+            this.label6.TabIndex = 8;
+            this.label6.Text = "σ   = ";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label5.Location = new System.Drawing.Point(3, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(226, 20);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "Обслуживающий автомат";
+            // 
+            // inputM
+            // 
+            this.inputM.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.inputM.Location = new System.Drawing.Point(65, 25);
+            this.inputM.Name = "inputM";
+            this.inputM.Size = new System.Drawing.Size(37, 26);
+            this.inputM.TabIndex = 5;
+            this.inputM.Text = "0";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label7.Location = new System.Drawing.Point(6, 31);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(46, 17);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "μ   = ";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(27, 261);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(509, 36);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Готово";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label11.Location = new System.Drawing.Point(123, 51);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(45, 17);
+            this.label11.TabIndex = 14;
+            this.label11.Text = "a < b";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label12.Location = new System.Drawing.Point(125, 73);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(55, 17);
+            this.label12.TabIndex = 15;
+            this.label12.Text = "σ  >  0";
+            // 
+            // error
+            // 
+            this.error.AutoSize = true;
+            this.error.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.error.ForeColor = System.Drawing.Color.Red;
+            this.error.Location = new System.Drawing.Point(24, 232);
+            this.error.Name = "error";
+            this.error.Size = new System.Drawing.Size(65, 15);
+            this.error.TabIndex = 3;
+            this.error.Text = "ОШИБКА";
+            this.error.Visible = false;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label8.Location = new System.Drawing.Point(30, 332);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(448, 18);
+            this.label8.TabIndex = 15;
+            this.label8.Text = "Максимальная длина очереди при пошаговом подходе:";
+            // 
+            // resTime
+            // 
+            this.resTime.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.resTime.Location = new System.Drawing.Point(483, 328);
+            this.resTime.Name = "resTime";
+            this.resTime.ReadOnly = true;
+            this.resTime.Size = new System.Drawing.Size(53, 26);
+            this.resTime.TabIndex = 16;
+            // 
+            // resEvent
+            // 
+            this.resEvent.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.resEvent.Location = new System.Drawing.Point(484, 369);
+            this.resEvent.Name = "resEvent";
+            this.resEvent.ReadOnly = true;
+            this.resEvent.Size = new System.Drawing.Size(53, 26);
+            this.resEvent.TabIndex = 18;
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label13.Location = new System.Drawing.Point(31, 373);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(458, 18);
+            this.label13.TabIndex = 17;
+            this.label13.Text = "Максимальная длина очереди при событийном подходе:";
+            // 
+            // label14
+            // 
+            this.label14.AutoSize = true;
+            this.label14.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label14.Location = new System.Drawing.Point(31, 428);
+            this.label14.Name = "label14";
+            this.label14.Size = new System.Drawing.Size(294, 16);
+            this.label14.TabIndex = 19;
+            this.label14.Text = "* ꟷ означает, что возникает переполнение";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(567, 489);
+            this.Controls.Add(this.label14);
+            this.Controls.Add(this.resEvent);
+            this.Controls.Add(this.label13);
+            this.Controls.Add(this.resTime);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.error);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.splitContainer1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel1.PerformLayout();
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            this.splitContainer1.Panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox inputA;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox inputB;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox inputS;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox inputM;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.TextBox inputReverse;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label error;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox resTime;
+        private System.Windows.Forms.TextBox resEvent;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.Label label14;
+    }
+}
+
---
+++ lab04/lab04/Form1.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Windows.Forms;
+using ComputingDevice;
+
+namespace lab04
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double a, b;
+            double m, s, rvr; 
+            int maxQueue = 0;
+            bool flag;
+            
+            Device device = new Device();
+
+
+            hideError();
+
+            try
+            {
+                getGenerator(out a, out b);
+                getAuto(out m, out s, out rvr);
+            }
+            catch (Exception err)
+            {
+                showError(err.Message);
+                return;
+            }
+
+            device.ProcessByTime(a, b, m, s, rvr, ref maxQueue, out flag);
+            fillRes(flag, maxQueue, "resTime");
+
+
+            device.ProcessByEvents(a, b, m, s, rvr, ref maxQueue, out flag);
+            fillRes(flag, maxQueue, "resEvent");
+        }
+
+        private void getGenerator(out double a, out double b)
+        {
+            string err = "ОШИБКА: неверный ввод параметров генератора";
+
+            try
+            {
+                a = double.Parse(this.inputA.Text);
+                b = double.Parse(this.inputB.Text);
+            }
+            catch
+            {
+                throw new Exception(err);
+            }
+
+            if (a >= b)
+                throw new Exception(err);
+        }
+
+        private void getAuto(out double m, out double s, out double rvr)
+        {
+            string err = "ОШИБКА: неверный ввод параметров обслуживающего автомата";
+            try
+            {
+                m = double.Parse(this.inputM.Text);
+                s = double.Parse(this.inputS.Text);
+                rvr = double.Parse(this.inputReverse.Text) / 100;     
+            }
+            catch
+            {
+                throw new Exception(err);
+            }
+
+            if (s <= 0 || rvr < 0 || rvr >= 1)
+                throw new Exception(err);
+        }
+
+        private void showError(string err)
+        {
+            this.error.Text = err;
+            this.error.Visible = true;
+        }
+
+        private void hideError()
+        {
+            this.error.Visible = false;
+        }
+
+        private void fillRes(bool flag, int maxQueue, string field)
+        {
+            if (flag)
+                this.Controls[field].Text = "   ꟷ";
+            else
+                this.Controls[field].Text = maxQueue.ToString();
+        }
+    }
+}
